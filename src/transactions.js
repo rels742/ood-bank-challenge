73d10e6deae96bfa71abadd169b88d3aaecbbf65
Lorel <lorel.oboue@gmail.com>
2022-06-17 03:46:52 +0100

    updated files

---
+++ src/transactions.js
@@ -0,0 +1,5 @@
+class Transactions {}
+
+module.exports = Transactions;
+
+// This class stores data for each bank account transations with a constrctor that when the class is called it displays the date, credit, debit, balance
